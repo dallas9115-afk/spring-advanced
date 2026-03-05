9a015bc3d43ebd2bd33a29015e718ca3a240606f
BKH <dallas9115@gmail.com>
2026-03-05 21:02:17 +0900

    레벨 3: @EntityGraph를 사용하여 Todo-User N+1 문제 해결

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -3,6 +3,7 @@ package org.example.expert.domain.todo.repository;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,7 +11,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
-
+    /*
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
@@ -20,4 +21,10 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 
     int countById(Long todoId);
+    */
+
+    // JPQL FETCH JOIN 대신 쓴 @EntityGraph 사용
+    @EntityGraph(attributePaths = {"user"})
+    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+
 }
